6747cb042e39168b86482e0a074d6abc1d7724a0
Raresco <trufin.rares@gmail.com>
2020-12-10 00:11:54 +0200

    Added classes

--- main.py
+++ main.py
@@ -3,10 +3,66 @@ import random
 
 pygame.init()  # Initalizare pygame
 
+
+class Dice:
+    @staticmethod
+    def roll_dice():
+        return random.randint(1, 6), random.randint(1, 6)
+
+    @staticmethod
+    def play_dice_animation():
+        pygame.mixer.Sound.play(dice_sound)
+        for i in range(0, 10):
+            dice_roll = Dice.roll_dice()
+            pygame.time.wait(20)
+            screen.blit(zaruri_png[dice_roll[0]], (20, 275))
+            screen.blit(zaruri_png[dice_roll[1]], (100, 275))
+            pygame.display.update()
+
+class Button:
+    def __init__(self, x_coordinate, y_coordinate, image, image_toggled, image_pressed):
+        self.button_state = 'normal'
+        self.png = image
+        self.png_toggled = image_toggled
+        self.png_pressed = image_pressed
+        self.x = x_coordinate
+        self.y = y_coordinate
+        self.width = image.get_size()[0]
+        self.height = image.get_size()[1]
+
+    def is_hovered_over(self):
+        if self.x < pygame.mouse.get_pos()[0] < (self.x + self.width) and \
+                self.y < pygame.mouse.get_pos()[1] < self.y + self.height:
+            self.button_state = 'hovered'
+        else:
+            self.button_state = 'normal'
+            return True
+
+    def is_pressed(self):
+        if self.button_state == 'hovered':
+            self.button_state = 'pressed'
+            return True
+
+    def display_button(self):
+        if self.button_state == 'normal':
+            screen.blit(self.png, (self.x, self.y))
+        elif self.button_state == 'hovered':
+            screen.blit(self.png_toggled, (self.x, self.y))
+        elif self.button_state == 'pressed':
+            screen.blit(self.png_pressed, (self.x, self.y))
+
+    @staticmethod
+    def play_sound():
+        pygame.mixer.Sound.play(pygame.mixer.Sound("sounds/button.wav"))
+        pygame.time.wait(300)
+
+
+dice_sound = pygame.mixer.Sound("sounds/dice_roll.wav")
+
 # Initializare ecran
-HEIGHT = 800
-WIDTH = 600
-screen = pygame.display.set_mode((HEIGHT, WIDTH))
+WIDTH = 800
+HEIGHT = 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 # Title and Icon
 pygame.display.set_caption("PyGammon")
@@ -35,10 +91,6 @@ die_4 = pygame.image.load("images/dice_4.png")
 die_5 = pygame.image.load("images/dice_5.png")
 die_6 = pygame.image.load("images/dice_6.png")
 
-
-# WAV's for menu buttons
-button_sound = pygame.mixer.Sound("sounds/button.wav")
-
 # Initialize Clock
 clock = pygame.time.Clock()
 clock.tick(60)
@@ -55,71 +107,68 @@ zaruri_png = {
     6: die_6
 }
 
-dice_rolled = (random.randint(1, 6), random.randint(1, 6))
-
-
-def main_menu():
-    global game_state, running
-    button1 = human_vs_human
-    button2 = human_vs_ai
-
-    button_pressed = 0
-    button1 = human_vs_human
-    button2 = human_vs_ai
-
-    # Highlight buttons on mouse-over
-    display_menu_background()
-    if 305 < pygame.mouse.get_pos()[0] < (305 + human_vs_human.get_width()) and 200 < pygame.mouse.get_pos()[1] < 245:
-        button1 = human_vs_human_toggled
-
-    if 328 < pygame.mouse.get_pos()[0] < 328 + human_vs_ai.get_width() and 300 < pygame.mouse.get_pos()[1] < 345:
-        button2 = human_vs_ai_toggled
-
-        # Check for events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-            pygame.quit()
-
-        if event.type == pygame.MOUSEBUTTONDOWN and button1 == human_vs_human_toggled:
-            button1 = human_vs_human_pressed
-            button_pressed = 1
-            game_state = "menu2"
-
-        elif event.type == pygame.MOUSEBUTTONDOWN and button2 == human_vs_ai_toggled:
-            button2 = human_vs_ai_pressed
-            button_pressed = 1
-            game_state = "menu2"
-
-        # Set coordinates and type of buttons for next screen display
-    screen.blit(button1, (305, 200))
-    screen.blit(button2, (328, 300))
-    pygame.display.update()
-
-    if button_pressed == 1:
-        pygame.mixer.Sound.play(button_sound)
-        pygame.time.wait(150)
-
+hvh_button = Button(305, 200, human_vs_human, human_vs_human_toggled, human_vs_human_pressed)
+hva_button = Button(328, 300, human_vs_ai, human_vs_ai_toggled, human_vs_ai_pressed)
 
 def display_menu_background():
     screen.blit(menu_background, (0, 0))
     screen.blit(logo, (300, 25))
 
 
-def player_vs_player_menu():
-    global running, dice_rolled
+def main_menu():
+    global game_state, running
+
+    # Highlight buttons on mouse-over
     display_menu_background()
-    screen.blit(board_image, (199, 0))
-    screen.blit(wood_texture, (0, 0))
+
+    if hvh_button.is_hovered_over():
+        pass
+
+    if hva_button.is_hovered_over():
+        pass
+
+        # Check for events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+
+            if hvh_button.is_pressed():
+                game_state = "menu2"
+                Button.play_sound()
+
+            elif hva_button.is_pressed():
+                game_state = "menu2"
+                Button.play_sound()
+
+    hvh_button.display_button()
+    hva_button.display_button()
+
+    pygame.display.update()
+
+
+def player_vs_player_menu(start):
+    global running
+    display_menu_background()
+    screen.blit(board_image, (199, 0))
+    screen.blit(wood_texture, (0, 0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                dice_rolled = (random.randint(1, 6), random.randint(1, 6))
-    screen.blit(zaruri_png[dice_rolled[0]], (20, 275))
-    screen.blit(zaruri_png[dice_rolled[1]], (100, 275))
+                Dice.play_dice_animation()
+                dice_rolled = Dice.roll_dice()
+                start = True
+
+    if start:
+        screen.blit(zaruri_png[dice_rolled[0]], (20, 275))
+        screen.blit(zaruri_png[dice_rolled[1]], (100, 275))
+        pygame.display.update()
 
 
 # Game Loop
@@ -128,7 +177,7 @@ while running:
     if game_state == "menu1":
         main_menu()
     elif game_state == "menu2":
-        player_vs_player_menu()
+        player_vs_player_menu(False)
 
     pygame.display.update()
 
